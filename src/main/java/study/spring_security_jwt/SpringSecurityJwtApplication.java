0454624618b1a93f7105bb264e69c50e52ef49c3
Jaehyuk Chang <cjaehyuk4zed@gmail.com>
2024-09-23 20:02:25 +0900

    :bulb: Test Gitmoji

--- src/main/java/study/spring_security_jwt/SpringSecurityJwtApplication.java
+++ src/main/java/study/spring_security_jwt/SpringSecurityJwtApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringSecurityJwtApplication {
 
+    // Random text to test Gitmoji commit - Can delete any time
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityJwtApplication.class, args);
     }
